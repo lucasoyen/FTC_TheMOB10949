59eda9c0b9a8625a6c2d7caa1a7f00c889f2ed54
slevine24 <slevine24@klschools.org>
2022-09-29 10:43:46 -0400

    Servo + Robot Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControlTestMaybeHopefully.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControlTestMaybeHopefully.java
@@ -54,17 +54,72 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name="Robot Control Test Maybe Hopefully", group="IDK")
 //@Disabled
 
-public class RobotControlTestMaybeHopefully extends LinearOpMode{
+public class RobotControlTestMaybeHopefully extends LinearOpMode {
     private Servo servo;
     private DcMotor motor;
+    private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor motor3;
+    private DcMotor motor4;
 
     @Override
     public void runOpMode() {
         servo = hardwareMap.get(Servo.class, "servo0");
         servo.setPosition(0);
         motor = hardwareMap.get(DcMotor.class, "motor4");
+        motor1 = hardwareMap.get(DcMotor.class, "frontLeft");
+        motor2 = hardwareMap.get(DcMotor.class, "frontRight");
+        motor3 = hardwareMap.get(DcMotor.class, "backRight");
+        motor4 = hardwareMap.get(DcMotor.class, "backLeft");
+
+        motor1.setDirection(DcMotor.Direction.FORWARD);
+
+        motor2.setDirection(DcMotor.Direction.REVERSE);
+
+        motor4.setDirection(DcMotor.Direction.FORWARD);
+
+        motor3.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+
+
+
+
+        motor4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+
+
+
+
+
+
+        MecanumDrive driveController = new MecanumDrive(motor1, motor2,motor4,motor2);
+
+
+
+        boolean toggleBack = false;
+
+        boolean toggleBack2 = false;
+
+        boolean x = false;
+
+        boolean pressable = true;
+
+        boolean pressable2 = true;
 
         telemetry.addData("Status", "Initialized");
+
+
+        telemetry.addData("Status", "Initialized");
+
+
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
@@ -75,8 +130,51 @@ public class RobotControlTestMaybeHopefully extends LinearOpMode{
 
             if (gamepad1.a) servo.setPosition(servo.getPosition() + 5);
             else if (gamepad1.b) servo.setPosition(servo.getPosition() - 5);
-            if(gamepad1.x) motor.setPower(1);
-            //if (gamepad1.left_stick_x == 5)
+            if (gamepad1.x) motor.setPower(1);
+            if (gamepad1.left_stick_x == 5) motor1.setPower(0.8);
+            else if (gamepad1.x){
+                motor2.setPower(1);
+                motor3.setPower(1);
+            }
+
+            else if (gamepad1.a && gamepad2.b) {
+                motor4.setPower(1);
+                motor3.setPower(1);
+                motor2.setPower(1);
+                motor1.setPower(1);
+            } else if (gamepad1.x && gamepad2.b) {
+                motor4.setPower(0);
+                motor3.setPower(0);
+                motor2.setPower(0);
+                motor1.setPower(0);
+            } else if (gamepad1.dpad_down) {
+                motor4.setPower(1);
+                motor3.setPower(-1);
+                motor2.setPower(-0.5);
+                motor1.setPower(-0.3);
+            }
+
+            double stickLx = this.gamepad1.left_stick_x;
+
+            double stickLy = this.gamepad1.left_stick_y;
+
+            double stickRx = this.gamepad1.right_stick_x;
+
+            boolean a = this.gamepad1.a;
+
+
+
+
+
+            double speed = 1;
+
+            driveController.moveInTeleop(stickLx, stickLy, stickRx,speed);
+
+            telemetry.addData("toggleB", toggleBack);
+
+            telemetry.addData("Status", "Running");
 
             telemetry.update();
+        }
+    }
 }

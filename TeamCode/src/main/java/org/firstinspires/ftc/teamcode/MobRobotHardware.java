025a4a12f15aeeb08f6da35bfac5d35ac1665fda
davidbond26 <bonddwb@gmail.com>
2022-10-03 10:48:12 -0400

    Robot Hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MobRobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MobRobotHardware.java
@@ -4,10 +4,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class MobRobotHardware {
-    public MobRobotHardware(HardwareMap hw) {
+    public MobRobotHardware() {
+
+    }
+
+    public void init(HardwareMap hw) {
         DcMotor frontLeft = hw.get(DcMotor.class, "frontLeft");
         DcMotor frontRight = hw.get(DcMotor.class, "frontRight");
         DcMotor backLeft = hw.get(DcMotor.class, "frontLeft");
         DcMotor backRight = hw.get(DcMotor.class, "frontLeft");
     }
+
 }

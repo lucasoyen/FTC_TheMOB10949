9bc27e719f3cfe5e29ba831ecf1793ebab858050
davidbond26 <bonddwb@gmail.com>
2022-11-17 19:21:43 -0500

    encoder stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTest.java
@@ -90,11 +90,11 @@ public class MecanumDriveTest extends LinearOpMode {
 
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
 
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
 
         backLeft.setDirection(DcMotor.Direction.FORWARD);
 
-        backRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
 
 
 

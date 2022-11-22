930f0debe469e57b8dd0c1a16312c3ffc7a1b721
loyen24 <loyen24@klschools.org>
2022-11-22 10:41:22 -0500

    basic arm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MRRangeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MRRangeTest.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-z
+
 
 @TeleOp(name="MRRangeTest", group="IDK")
 

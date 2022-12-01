5f98b3cc51e5ecf14ae6fbca0ab709e3e4b46826
davidbond26 <bonddwb@gmail.com>
2022-12-01 10:54:47 -0500

    cameratest and update cucumogorange

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
+package org.firstinspires.ftc.teamcode;/* Copyright (c) 2019 FIRST. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted (subject to the limitations in the disclaimer below) provided that
@@ -27,17 +27,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 import java.util.List;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -55,8 +49,8 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@Autonomous
-public class  Cucumogorange extends LinearOpMode {
+@Autonomous(name = "CameraTest", group = "Your mother")
+public class CameraTest extends LinearOpMode {
 
     /*
      * Specify the source for the Tensor Flow Model.
@@ -65,15 +59,15 @@ public class  Cucumogorange extends LinearOpMode {
      * has been downloaded to the Robot Controller's SD FLASH memory, it must to be loaded using loadModelFromFile()
      * Here we assume it's an Asset.    Also see method initTfod() below .
      */
-//    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
-//    private static final String TFOD_MODEL_FILE  = "/Environment.getExternalStorageDirectory().getPath()/FIRST/tflitemodels/CustomTeamModel.tflite";
-    private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/amogus.tflite";
+//    private static final String TFOD_MODEL_ASSET = "Cucumogorange.tflite";
+     private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/Cucumogorange.tflite";
+
+
     private static final String[] LABELS = {
             "amogus",
             "cucumber",
             "orange"
     };
-
 
     /*
      * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
@@ -101,46 +95,11 @@ public class  Cucumogorange extends LinearOpMode {
      * Detection engine.
      */
     private TFObjectDetector tfod;
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
-    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-
-        boolean done = false;
-
-        frontLeft.setDirection(DcMotor.Direction.FORWARD);
-
-        frontRight.setDirection(DcMotor.Direction.FORWARD);
-
-        backLeft.setDirection(DcMotor.Direction.FORWARD);
-
-        backRight.setDirection(DcMotor.Direction.FORWARD);
-
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
         initVuforia();
         initTfod();
 
@@ -173,41 +132,21 @@ public class  Cucumogorange extends LinearOpMode {
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                     if (updatedRecognitions != null) {
                         telemetry.addData("# Objects Detected", updatedRecognitions.size());
-                        telemetry.update();
+
                         // step through the list of recognitions and display image position/size information for each one
                         // Note: "Image number" refers to the randomized image orientation/number
-                        if (!done && updatedRecognitions.size() > 0) {
-                            Recognition highest = null;
-                            for (Recognition recognition : updatedRecognitions) {
-                                double col = (recognition.getLeft() + recognition.getRight()) / 2;
-                                double row = (recognition.getTop() + recognition.getBottom()) / 2;
-                                double width = Math.abs(recognition.getRight() - recognition.getLeft());
-                                double height = Math.abs(recognition.getTop() - recognition.getBottom());
+                        for (Recognition recognition : updatedRecognitions) {
+                            double col = (recognition.getLeft() + recognition.getRight()) / 2 ;
+                            double row = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+                            double width  = Math.abs(recognition.getRight() - recognition.getLeft()) ;
+                            double height = Math.abs(recognition.getTop()  - recognition.getBottom()) ;
 
-                                telemetry.addData("", " ");
-                                telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
-                                telemetry.addData("- Position (Row/Col)", "%.0f / %.0f", row, col);
-                                telemetry.addData("- Size (Width/Height)", "%.0f / %.0f", width, height);
-                                telemetry.update();
-                                if (highest == null || highest.getConfidence() < recognition.getConfidence()) highest = recognition;
-                            }
-
-                            double forwardDist = 0.008;
-                            double sideDist = 0.008;
-                            double time = 2;
-                            double speed = 0.2;
-
-                            if (highest.getLabel().equals("cucumber")) {
-                                encoderRight(sideDist, speed, time);
-                            } else if (highest.getLabel().equals("orange")) {
-                                encoderLeft(sideDist, speed, time);
-                            }
-                            encoderForward(forwardDist, speed, time);
-
-                            done = true;
-                            tfod.deactivate();
-                            telemetry.update();
+                            telemetry.addData(""," ");
+                            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100 );
+                            telemetry.addData("- Position (Row/Col)","%.0f / %.0f", row, col);
+                            telemetry.addData("- Size (Width/Height)","%.0f / %.0f", width, height);
                         }
+                        telemetry.update();
                     }
                 }
             }
@@ -237,7 +176,7 @@ public class  Cucumogorange extends LinearOpMode {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.6f;
+        tfodParameters.minResultConfidence = 0.3f;
         tfodParameters.isModelTensorFlow2 = true;
         tfodParameters.inputSize = 300;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
@@ -245,90 +184,6 @@ public class  Cucumogorange extends LinearOpMode {
         // Use loadModelFromAsset() if the TF Model is built in as an asset by Android Studio
         // Use loadModelFromFile() if you have downloaded a custom team model to the Robot Controller's FLASH.
 //        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
-        tfod.loadModelFromFile(TFOD_MODEL_FILE, LABELS);
-    }
-
-    private void encoderDrive(double fl, double fr, double bl, double br, double speed, double time) {
-        int frontLeftTarget = (int)(fl * 1400);
-        int frontRightTarget = (int)(fr * 1400);
-        int backLeftTarget = (int)(bl * 1400);
-        int backRightTarget = (int)(br * 1400);
-
-        frontLeft.setTargetPosition(frontLeftTarget);
-        frontRight.setTargetPosition(frontRightTarget);
-        backLeft.setTargetPosition(backLeftTarget);
-        backRight.setTargetPosition(backRightTarget);
-
-        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        frontLeft.setPower(Math.abs(speed));
-        frontRight.setPower(Math.abs(speed));
-        backLeft.setPower(Math.abs(speed));
-        backRight.setPower(Math.abs(speed));
-
-        runtime.reset();
-
-        while (opModeIsActive() && runtime.seconds() < time && (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy())) {
-            telemetry.addData("Running to", "\nBackLeft: " + backLeftTarget + "\nBackRight: " + backRightTarget + "\nFrontRight: " + frontRightTarget + "\nFrontLeft: " + frontLeftTarget);
-            telemetry.addData("Currently at", backLeft.getCurrentPosition() + ", " + backRight.getCurrentPosition() + ", " + frontRight.getCurrentPosition() + ", " + frontLeft.getCurrentPosition());
-            telemetry.update();
-        }
-
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backRight.setPower(0);
-        backLeft.setPower(0);
-
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-    private void encoderRight(double dist, double speed, double time) {
-        encoderDrive(dist, -dist, -dist, dist, speed, time);
-    }
-    private void encoderLeft(double dist, double speed, double time) {
-        encoderDrive(-dist, dist, dist, -dist, speed, time);
-    }
-    private void encoderBack(double dist, double speed, double time) {
-        encoderDrive(-dist, -dist, -dist, -dist, speed, time);
-    }
-    private void encoderForward(double dist, double speed, double time) {
-        encoderDrive(dist, dist, dist, dist, speed, time);
-    }
-    private void move(double fl, double fr, double bl, double br, double time, double speed) {
-
-        frontLeft.setPower(fl * Math.abs(speed));
-        frontRight.setPower(fr * Math.abs(speed));
-        backLeft.setPower(bl * Math.abs(speed));
-        backRight.setPower(br * Math.abs(speed));
-
-        runtime.reset();
-
-        while (opModeIsActive() && runtime.seconds() < time) {
-            telemetry.addData("Running to", "\nBackLeft: " + bl + "\nBackRight: " + br + "\nFrontRight: " + fr + "\nFrontLeft: " + fl);
-            telemetry.addData("Currently at", backLeft.getCurrentPosition() + ", " + backRight.getCurrentPosition() + ", " + frontRight.getCurrentPosition() + ", " + frontLeft.getCurrentPosition());
-            telemetry.update();
-        }
-
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backRight.setPower(0);
-        backLeft.setPower(0);
-    }
-    private void moveRight(double dist, double speed, double time) {
-        move(dist, -dist, -dist, dist, speed, time);
-    }
-    private void moveLeft(double dist, double speed, double time) {
-        move(-dist, dist, dist, -dist, speed, time);
-    }
-    private void moveBack(double dist, double speed, double time) {
-        move(-dist, -dist, -dist, -dist, speed, time);
-    }
-    private void moveForward(double dist, double speed, double time) {
-        move(dist, dist, dist, dist, speed, time);
+         tfod.loadModelFromFile(TFOD_MODEL_FILE, LABELS);
     }
 }
